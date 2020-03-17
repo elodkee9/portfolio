4a6a3825bd7a9e4e968cfd10f29996515f330e01
Mihaly Elod <elodkee99@yahoo.com>
2020-03-17 17:14:44 +0200

    Updates

---
+++ precache-manifest.e91c7d8d43b7e799ef7f02a3e969257e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55d205b040670f33035ed3339232275",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "27c36e92cb10650a7eba",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c467930f77954bdb4e1",
+    "url": "/portfolio/static/js/2.92cbde7f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.92cbde7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c36e92cb10650a7eba",
+    "url": "/portfolio/static/js/main.02d98c82.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
